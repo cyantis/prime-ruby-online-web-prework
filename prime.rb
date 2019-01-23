9683453e8dacc205ac5986bab233725efd3c78ff
Cody Yantis <cyantis@gmail.com>
2019-01-23 23:42:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(i)
+  counter = (i..0).to_a
+  while i > 0
+    counter.each do |divisor|
+      if i % divisor == 0
+        false
+        break
+    end
+  end
+  return true
+end
